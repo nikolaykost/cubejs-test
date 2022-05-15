360a7f592a36b4fd4d0c66645bb91669611171fc
system <system@cubecloud.dev>
2022-05-15 14:11:25 +0000

    Test

---
+++ schema/test.js
@@ -0,0 +1,20 @@
+cube('test', {
+  sql: ``,
+
+  preAggregations: {
+    // Pre-Aggregations definitions go here
+    // Learn more here: https://cube.dev/docs/caching/pre-aggregations/getting-started
+  },
+
+  joins: {
+
+  },
+
+  measures: {
+
+  },
+
+  dimensions: {
+
+  }
+});
