874547afced2779d453ddcb65cc37b0f8c76b52b
nikolay <nikolay@cube.dev>
2022-08-17 16:43:04 +0000

    security context

--- cube.js
+++ cube.js
@@ -1,1 +1,6 @@
-module.exports={}; // from test 0.7183600933791277
+module.exports={
+  scheduledRefreshContexts: async () => [
+  { securityContext: { tenant: 'development' } },
+  { securityContext: { tenant: 'production' } },
+],
+}; // from test 0.7183600933791277
