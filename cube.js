98f28c350f5da42f993856bddefd4e115c07281f
nikolay <nikolay@cube.dev>
2022-08-30 11:45:24 +0000

    add security contexts

--- cube.js
+++ cube.js
@@ -2,5 +2,9 @@ module.exports={
   scheduledRefreshContexts: async () => [
   { securityContext: { tenant: 'development' } },
   { securityContext: { tenant: 'production' } },
+  { securityContext: { tenant: 'test' } },
+  { securityContext: { tenant: 'valid' } },
+  { securityContext: { tenant: 'invalid' } },
+  { securityContext: { tenant: 'part' } },
 ],
 }; // from test 0.7183600933791277
